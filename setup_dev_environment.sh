7d5c58228d56d01dea0606f84769c69bb3766ed8
nicolaskyejo <36485974+rokkafella3@users.noreply.github.com>
2020-01-01 15:59:07 +0200

    added initial git config

--- setup_dev_environment.sh
+++ setup_dev_environment.sh
@@ -7,18 +7,21 @@
 
 if type "yum" > /dev/null; then
     sudo yum -y update
-	sudo yum install -y curl zsh
+	sudo yum install -y curl zsh git vim
 elif type "dnf" > /dev/null; then
 	sudo dnf -y update
-	sudo dnf install -y curl zsh
+	sudo dnf install -y curl zsh git vim
 elif type "apt" > /dev/null; then	
 	sudo apt update && sudo apt -y upgrade
-	sudo apt install -y curl zsh
+	sudo apt install -y curl zsh git vim
 elif type "pacman" > /dev/null; then	
 	sudo pacman -Syu --noconfirm
-	sudo pacman -S --noconfirm curl zsh
+	sudo pacman -S --noconfirm curl zsh git vim
 fi
 
+git config --global user.name "Nicolas Kyejo"
+git config --global user.email "nicolaskyejo@users.noreply.github.com"
+git config --global core.editor "vim"
 chsh -s $(which zsh)  # changing default shell
 mkdir -p ~/bin &&
 curl -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr  # installing tldr
@@ -26,7 +29,7 @@ chmod +x ~/bin/tldr
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"  # installing ohmyzsh, no need to set as zsh as default as it has been already set
 zsh
 sed -i '2 s/^# //' ~/.zshrc  # uncomment 2nd line
-echo 'x="exit"' >> ~/.zshrc
+printf 'x="exit"\nc="clear"\nv="vim"\npdf="pdflatex -interaction=nonstopmode"\nlpdf="lualatex -interaction=nonstopmode"\nxpdf="xelatex -interaction=nonstopmode"\n' >> ~/.zshrc
 source ~/.zshrc
 complete -W "$(tldr 2>/dev/null --list)" tldr  # autocomplete for tldr
 sed -i 's/plugins=(git)/plugins=(git autopep8 python)/' ~/.zshrc  # adding plugins
